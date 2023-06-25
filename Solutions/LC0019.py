2ca26a7e7720d4835cfc1df2ec8be3bfdbbd7065
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-06-25 17:49:47 +0530

    Update LC0019.py

--- Solutions/LC0019.py
+++ Solutions/LC0019.py
@@ -1,23 +1,23 @@
 class Solution:
-    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
-        n = len(locations)
+    MOD = 1000000007
 
-        dp = {}
-        def solve(currCity, remainingFuel):
-            if remainingFuel < 0:
-                return 0
-            if (currCity, remainingFuel) in dp:
-                return dp[(currCity, remainingFuel)]
-
-            ans = 0
-            if currCity == finish:
-                ans = 1
-            for nextCity in range(n):
-                if nextCity != currCity:
-                    ans = (ans + solve(nextCity, remainingFuel - abs(
-                        locations[currCity] - locations[nextCity]))) % 1000000007
-            
-            dp[(currCity, remainingFuel)] = ans
-            return ans
+    def solve(self, locations, city, finish, remainFuel, memo):
+        if remainFuel < 0:
+            return 0
         
-        return solve(start, fuel)
+        if memo[city][remainFuel] is not None:
+            return memo[city][remainFuel]
+        
+        total = 1 if city == finish else 0
+        
+        for nextCity in range(len(locations)):
+            if nextCity != city and remainFuel >= abs(locations[nextCity] - locations[city]):
+                total = (total + self.solve(locations, nextCity, finish, remainFuel - abs(locations[nextCity] - locations[city]), memo)) % self.MOD
+        
+        memo[city][remainFuel] = total
+        return total
+
+    def countRoutes(self, locations, start, finish, fuel):
+        n = len(locations)
+        memo = [[None] * (fuel + 1) for _ in range(n)]
+        return self.solve(locations, start, finish, fuel, memo)
