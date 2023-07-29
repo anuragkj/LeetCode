7373662b4ced6bbca60290fa35d98167db0a385b
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-07-29 13:07:26 +0530

    name fix

--- Solutions/LC0052.py
+++ Solutions/LC0052.py
@@ -1,19 +1,23 @@
 class Solution:
-    def PredictTheWinner(self, nums: List[int]) -> bool:
-        n = len(nums)
-        if n%2 ==0: #Check if even or odd series is better, force the opponent to choose other
-            return True
-        memo = collections.defaultdict(int)
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        # Get the sum of all extra batteries.
+        batteries.sort()   
+        extra = sum(batteries[:-n])
         
-        def maxDiff(left, right):
-            if (left, right) in memo:
-                return memo[(left, right)]
-            if left == right:
-                return nums[left]
-            score_by_left = nums[left] - maxDiff(left + 1, right)
-            score_by_right = nums[right] - maxDiff(left, right - 1)
+        # live stands for the n largest batteries we chose for n computers.
+
+        live = batteries[-n:]
+        
+        # We increase the total running time using 'extra' by increasing 
+        # the running time of the computer with the smallest battery.
+        for i in range(n - 1):
+            # If the target running time is between live[i] and live[i + 1].
+            if extra // (i + 1) < live[i + 1] - live[i]:
+                return live[i] + extra // (i + 1)
             
-            memo[(left, right)] = max(score_by_left, score_by_right)
-            return memo[(left, right)]
+            # Reduce 'extra' by the total power used.
+            extra -= (i + 1) * (live[i + 1] - live[i])
         
-        return maxDiff(0, n - 1) >= 0
+        # If there is power left, we can increase the running time 
+        # of all computers.
+        return live[-1] + extra // n
--- Solutions/LC0053.py
+++ Solutions/LC0053.py
@@ -1,20 +1,19 @@
 class Solution:
-    def soupServings(self, n: int) -> float:
-        if n > 4800: 
-            return 1.0
-        n = (n + 24) // 25
-        memo = dict()
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n%2 ==0: #Check if even or odd series is better, force the opponent to choose other
+            return True
+        memo = collections.defaultdict(int)
         
-        def dp(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if i <= 0 and j <= 0: 
-                return 0.5
-            if i <= 0: 
-                return 1.0
-            if j <= 0: 
-                return 0.0
-            memo[(i, j)] = 0.25 * (dp(i - 4, j) + dp(i - 3, j - 1) + dp(i - 2, j - 2) + dp(i - 1, j - 3))
-            return memo[(i, j)]
+        def maxDiff(left, right):
+            if (left, right) in memo:
+                return memo[(left, right)]
+            if left == right:
+                return nums[left]
+            score_by_left = nums[left] - maxDiff(left + 1, right)
+            score_by_right = nums[right] - maxDiff(left, right - 1)
+            
+            memo[(left, right)] = max(score_by_left, score_by_right)
+            return memo[(left, right)]
         
-        return dp(n, n)
+        return maxDiff(0, n - 1) >= 0
