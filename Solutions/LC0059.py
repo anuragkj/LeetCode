5834a37ef77a6f7fefd2647fcdfdfab675f6898d
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-08-03 18:33:53 +0530

    Create LC0059.py

---
+++ Solutions/LC0059.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        phone_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        combinations = [""]
+
+        for digit in digits:
+            new_combinations = []
+            for combination in combinations:
+                for letter in phone_map[digit]:
+                    new_combinations.append(combination + letter)
+            combinations = new_combinations
+
+        return combinations
