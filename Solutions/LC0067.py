b49ac01a9c26829492be0739b96cbddc182c9157
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-08-10 13:07:30 +0530

    Create LC0067.py

---
+++ Solutions/LC0067.py
@@ -0,0 +1,35 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        n = len(nums)  # size of the numsay
+        low, high = 0, n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            # if mid points to the target
+            if nums[mid] == target:
+                return True
+
+            # Edge case:
+            if nums[low] == nums[mid] and nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+
+            # if left part is sorted
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    # element exists
+                    high = mid - 1
+                else:
+                    # element does not exist
+                    low = mid + 1
+            else:  # if right part is sorted
+                if nums[mid] <= target <= nums[high]:
+                    # element exists
+                    low = mid + 1
+                else:
+                    # element does not exist
+                    high = mid - 1
+
+        return False
