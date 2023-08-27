4f44cd57c2cb6f79b84eaa7994d3b72a3c3f7349
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-08-27 12:32:58 +0530

    Create LC0084.py

---
+++ Solutions/LC0084.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+
+        if stones[1] != 1:
+            return False
+
+        def dfs(i, k):
+            if i == len(stones) - 1:
+                return True
+
+            if (i, k) in dp:
+                 return dp[(i, k)]
+
+            res = False
+            for j in range(i + 1, len(stones)):
+                if stones[i] + k == stones[j]:
+                    res = res or dfs(j, k)
+                if stones[i] + k + 1 == stones[j]:
+                    res = res or dfs(j, k + 1)
+                if stones[i] + k - 1 == stones[j]:
+                    res = res or dfs(j, k - 1)
+
+            dp[(i, k)] = res
+            return res
+        
+        dp = {}
+        return dfs(1, 1)
