9aedd18ddbb81a89d63b221ca9817ccb81839625
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-10-04 07:55:58 +0400

    Create LC0122.py

---
+++ Solutions/LC0122.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        counts = defaultdict(int)
+        ans = 0
+        
+        for num in nums:
+            ans += counts[num]
+            counts[num] += 1
+        
+        return ans
