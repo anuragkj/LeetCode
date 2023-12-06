7b2a3d11fa9b4c20e5bfc71ceac92dd8769b4b7c
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2023-12-06 17:07:53 +0400

    Update LC0186.py

--- Solutions/LC0186.py
+++ Solutions/LC0186.py
@@ -4,8 +4,8 @@ class Solution:
         complete_week = n // 7
         remaining_days = n % 7
 
-        total += (28 * complete_week) + (7 * (complete_week - 1) * complete_week // 2) [#total](https://leetcode.com/problems/total-hamming-distance) for complete weeks
+        total += (28 * complete_week) + (7 * (complete_week - 1) * complete_week // 2) 
 
-        total += (complete_week + 1 + complete_week + remaining_days) * remaining_days // 2 [#total](https://leetcode.com/problems/total-hamming-distance) for remaining days
+        total += (complete_week + 1 + complete_week + remaining_days) * remaining_days // 2
 
         return total
