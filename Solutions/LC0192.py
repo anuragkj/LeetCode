a3ef41063e4d69e31fe656d4a0da723728c6e0dc
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2023-12-12 11:18:10 +0400

    Create LC0192.py

---
+++ Solutions/LC0192.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        biggest = 0
+        second_biggest = 0
+        for num in nums:
+            if num > biggest:
+                second_biggest = biggest
+                biggest = num
+            else:
+                second_biggest = max(second_biggest, num)
+        
+        return (biggest - 1) * (second_biggest - 1)
