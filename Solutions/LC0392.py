304af4f5b73a14b8bc0950790cd016881f7637c7
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2024-06-29 20:15:28 +0400

    Create LC0392.py

---
+++ Solutions/LC0392.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        first_edge, second_edge = edges[0], edges[1]
+
+        return first_edge[0] if first_edge[0] in second_edge else first_edge[1]
