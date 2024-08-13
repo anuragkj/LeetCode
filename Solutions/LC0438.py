9ce4f6c7becdb47ac7eb47f585c7e63c6a3d880e
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2024-08-13 22:34:44 +0400

    Create LC0438.py

---
+++ Solutions/LC0438.py
@@ -0,0 +1,16 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.min_heap = []
+        self.k = k
+
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        # Add to our min_heap if we haven't processed k elements yet
+        # or if val is greater than the top element (the k-th largest)
+        if len(self.min_heap) < self.k or self.min_heap[0] < val:
+            heapq.heappush(self.min_heap, val)
+            if len(self.min_heap) > self.k:
+                heapq.heappop(self.min_heap)
+        return self.min_heap[0]
