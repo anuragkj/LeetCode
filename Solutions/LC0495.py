6dd384eccb9638e9b5e53d722673694b7e6bcbc4
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2024-10-09 19:59:11 +0400

    Create LC0495.py

---
+++ Solutions/LC0495.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_brackets = 0
+        min_adds_required = 0
+
+        for c in s:
+            if c == "(":
+                open_brackets += 1
+            else:
+                if open_brackets > 0:
+                    open_brackets -= 1
+                else:
+                    min_adds_required += 1
+
+        return min_adds_required + open_brackets
